8c61d0050deb812140bdf5de2a8e87c5d4e5d6cd
Adithya V <venkateshadithya007@gmail.com>
2024-07-17 20:34:48 +0530

    Updated dir structure and setup db connection

---
+++ backend/database/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'product_management_system'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id ' + connection.threadId);
+});
+
+module.exports = connection;
